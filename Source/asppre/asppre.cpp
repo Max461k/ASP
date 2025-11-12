b161128ced59f08d73044b08b241e8e1b7b776c3
Max461k <maxim.bartashov@gmail.com>
2025-11-12 14:13:30 +0100

    iniselise

---
+++ Source/asppre/asppre.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "asppre.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, asppre, "asppre" );
